904b46c81dd57c8f94126b603f330162197fd99a
가입dldidsla13 <dldidsla13@naver.com>
2023-09-14 11:37:06 +0900

    BoardController

--- src/main/java/controllers/BoradController.java
+++ src/main/java/controllers/BoradController.java
@@ -7,11 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("*.member")
-public class MemberController extends HttpServlet {
+@WebServlet("*board")
+public class BoradController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
